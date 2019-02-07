0b51226667a76b160ece7376afd7d3f1d73e0ac9
epicGG17 <47368424+epicGG17@users.noreply.github.com>
2019-02-07 13:57:37 -0800

    G change

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -6,18 +6,22 @@
   @return: string representing the integer value.
 */
 std::string FizzBuzz::stringifier(int i) {
-
-    return std::to_string(i);
-
+  
 }
 
 /*
   Returns the appropiate string based on the number given.
   @parameter: int i is our given number.
   @return: string representing i, unless i is divisible by 3,
-   then return "Fizz", or if i is divisible by 5 return "Buzz"
+   then return "Fizz", or if i is divisible by 5 return "Buzz" 
    or if i is divisible by 3 and 5 return "FizzBuzz".
 */
 std::string FizzBuzz::evaluate(int i) {
-
+  if(fizz.isWorthy(i) && buzz.isWorthy(i))
+    return "FizzBuzz";
+  if(fizz.isWorthy(i))
+    return "Fizz";
+  if(buzz.isWorthly(i))
+    return "Buzz";
+  return stringifier(i);
 }
