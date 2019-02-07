e25c926b0884e15f139d0feeb665ac6f73030385
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-02-07 13:36:12 -0800

    Defined FizzBuzz::stringifier() function

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -6,16 +6,18 @@
   @return: string representing the integer value.
 */
 std::string FizzBuzz::stringifier(int i) {
-  
+
+    return std::to_string(i);
+
 }
 
 /*
   Returns the appropiate string based on the number given.
   @parameter: int i is our given number.
   @return: string representing i, unless i is divisible by 3,
-   then return "Fizz", or if i is divisible by 5 return "Buzz" 
+   then return "Fizz", or if i is divisible by 5 return "Buzz"
    or if i is divisible by 3 and 5 return "FizzBuzz".
 */
 std::string FizzBuzz::evaluate(int i) {
-  
+
 }
